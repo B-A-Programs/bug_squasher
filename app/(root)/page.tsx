f3b9391e9832dae53941bb6663a943a65462f934
Andrei Tudorache <tudoracheandrei09@gmail.com>
2024-02-12 19:55:02 +0200

    Hero page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/shared/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      
+    <main className="flex-1 justify-center items-center">
+      <Hero />
     </main>
   );
 }
--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import { Button } from "../ui/button"
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-import NavItems from "./NavItems"
 import Link from "next/link"
 
 const NavBar = () => {
@@ -9,12 +8,9 @@ const NavBar = () => {
     <nav className="wrapper flex items-center justify-between">
       <h1 className="flex justify-center items-center flex-row text-xl font-extrabold">
         <Image src="/assets/bug_icon.jpg" alt="bug icon" width={40} height={40} />
-        <span className="text-indigo-600">Bug</span><span className="text-purple-600">Squasher</span>
+        <span className="purple_gradient">BugSquasher</span>
       </h1>
       <SignedIn>
-        <nav className="md:flex-between hidden w-full max-w-xs">
-          <NavItems />
-        </nav>
       </SignedIn>
       <SignedIn>
         <UserButton afterSignOutUrl="/" />
